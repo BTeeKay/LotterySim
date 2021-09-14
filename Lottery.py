3ae1f9cdb31b0523f67279dce3ef4021bd589eaa
Brian Kerr <briantkerr91@gmail.com>
2021-09-14 17:05:03 +0100

    Updated formatting and added comments

--- Lottery.py
+++ Lottery.py
@@ -9,11 +9,13 @@ def main():
     getnumbers(num)
     print("Thank you for entering your numbers")
     print("Let's see if you are lucky today")
+    # Populate Winners list with random numbers, using getrandom function
     winners = []
     for i in range(len(num)):
         winners.append(getrandom())
     print(f"The Winning Numbers are: {winners}")
 
+    # print and count matching numbers
     x = 0
     for j in range(len(winners)):
         if winners[j] == num[j]:
@@ -22,14 +24,14 @@ def main():
 
     print(f"You got {x} matches")
 
+
 def getnumbers(num):
 
-
+    # Get int input from the user within a certain range
     for i in range(6):
         z = cs50.get_int(f"Number {i + 1}: ")
         while z < 0 or z > 4:
             z = cs50.get_int(f"Number {i + 1}: ")
-
 
         num.append(z)
 
@@ -38,7 +40,8 @@ def getnumbers(num):
 
 def getrandom():
 
-    x = random.randrange(1,3)
+    # generate random number and return value
+    x = random.randrange(1, 3)
     return x
 
 
